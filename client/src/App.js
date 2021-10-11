8dbc731d25f929344e636968356ebb78d3030616
Dipankar-Medhi <dipumedhi99@gmail.com>
2021-10-11 12:45:02 +0530

    added footer

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from "./components/home/home";
 import NavBar from "./components/navbar/navbar";
 import Signup from "./components/signup/Signup";
 import Order from "./components/Order/Order";
+import Footer from "./components/Footer/Footer";
 function App() {
   return (
     <>
@@ -12,10 +13,11 @@ function App() {
         <NavBar />
         <Switch>
           <Route path="/" exact component={Home} />
-          <Route path='/signin' component={Signup}/>
+          <Route path='/signin' component={Signup} />
           {/* define all the routes here */}
-          <Route path='/order' component={Order}/>
+          <Route path='/order' component={Order} />
         </Switch>
+        <Footer />
       </Router>
     </>
   );
